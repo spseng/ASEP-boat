c541e28fce32643b33e0d832a05cd6b0fe512b9d
Motoki Nakajima <motokinakajima0912@gmail.com>
2026-09-16 15:18:05 -0400

    Add parser

---
+++ shared/serial_protocol/include/parser.h
@@ -0,0 +1,11 @@
+#include <cstddef>
+#include <cstdint>
+
+class Parser {
+public:
+    Parser();
+
+    bool feed(uint8_t b);
+private:
+
+};
---
+++ shared/serial_protocol/include/types.h
@@ -0,0 +1,10 @@
+#include <cstdint>
+
+namespace protocol {
+    struct Header {
+        uint8_t type;
+        uint8_t length;
+    };
+
+    
+}
